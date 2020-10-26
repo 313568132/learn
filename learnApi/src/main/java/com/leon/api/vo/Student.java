0ad6a66b5b78496f9ea558f28d0b0916a31eed1c
leon <313568132@qq.com>
2020-10-26 17:49:24 +0800

    添加一个student类

---
+++ learnApi/src/main/java/com/leon/api/vo/Student.java
@@ -0,0 +1,11 @@
+package com.leon.api.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    public String name;
+    public Integer age;
+}
