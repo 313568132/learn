0336c1878407386d285a047b6ba60090a8bd1951
leon <313568132@qq.com>
2020-11-16 22:36:38 +0800

    添加log aop

--- springLearn/src/main/java/com/leon/springl/aop/LogAop.java
+++ springLearn/src/main/java/com/leon/springl/aop/LogAop.java
@@ -2,11 +2,13 @@ package com.leon.springl.aop;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletRequest;
@@ -19,7 +21,7 @@ import java.util.List;
 @Slf4j
 public class LogAop {
 
-    @Pointcut("execution(* com.leon.springl.service.*.*(..))")
+    @Pointcut("execution(* com.leon.springl.service.UserService.*(..))")
     public void logPoint(){}
 
     @Around("logPoint()")
@@ -46,5 +48,28 @@ public class LogAop {
             throw throwable;
         }
         return proceed;
+    }
+
+    @Pointcut("execution(* com.leon.springl.service.AnimalService.*(..))")
+    public void allPoint(){}
+
+    @Before("allPoint()")
+    public void aopBefore(){
+        System.out.println("LogAop aopBefore()......");
+    }
+
+    @After("allPoint()")
+    public void aopAfter(){
+        System.out.println("LogAop aopAfter()......");
+    }
+
+    @AfterReturning(pointcut = "allPoint()",returning = "o")
+    public void aopAfterReturning(Object o){
+        System.out.println("LogAop aopAfterReturning()......" + o.toString());
+    }
+
+    @AfterThrowing(pointcut = "allPoint()",throwing = "e")
+    public void aopAfterReturning(Exception e){
+        System.out.println("LogAop aopAfterReturning()......" + e);
     }
 }
