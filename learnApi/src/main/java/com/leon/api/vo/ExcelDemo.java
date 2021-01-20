ed0ca1a7075f4540fdad389e196a87b13c3991e1
leon <313568132@qq.com>
2021-01-20 10:46:20 +0800

    添加阿里easyexcel.

---
+++ learnApi/src/main/java/com/leon/api/vo/ExcelDemo.java
@@ -0,0 +1,26 @@
+package com.leon.api.vo;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.Data;
+
+@Data
+public class ExcelDemo {
+    // sku	描述	英文描述	申报品名	链接	hscode	清关国家	清关模式	品类
+    @ExcelProperty("merchatSku")
+    public String merchatSku;
+    @ExcelProperty("itemDesc")
+    public String itemDesc;
+    @ExcelProperty("itemDescEn")
+    public String itemDescEn;
+    public String itemDescDeclear;
+    public String hscode;
+    @ExcelProperty("category")
+    public String category;
+    @ExcelProperty("itemUrl")
+    public String itemUrl;
+    public String esMerchatSku;
+    public String esMerchatId;
+    public String remark;
+    public String remark2;
+    public String remark3;
+}
